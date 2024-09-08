b6c01409fcfc622c3b358a347dbf992b115ae3d0
PaulSPuntusov <pps_ru@mail.ru>
2024-09-08 21:22:47 +0300

    третья переделка

---
+++ src/LogEntry.java
@@ -0,0 +1,52 @@
+import java.time.LocalDateTime;
+import java.time.Month;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+
+
+
+public class LogEntry {
+    java.util.Locale locale = Locale.UK;
+    public final String line;
+    public final String ipAddr;
+    public final LocalDateTime time;
+
+    public LogEntry(String line) {
+        this(line, "", LocalDateTime.of(2000, Month.SEPTEMBER, 20, 8, 8, 11));
+    }
+
+    public LogEntry(String line, String ipAddr, LocalDateTime time) {
+        this.line = line;
+        this.ipAddr = ip();
+        this.time = ldt();
+    }
+
+
+    // парсим IP
+    public String ip() {
+        Pattern p = Pattern.compile("\\b\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}\\b");
+        Matcher m = p.matcher(this.line);
+        return m.find() ? m.group() : "Not found";
+    }
+
+    // парсим дату_время по квадратным скабкам
+    public LocalDateTime ldt() {
+        String date_time;
+        LocalDateTime dl;
+        Pattern p = Pattern.compile("[\\[](.*?)[\\]]"); // нагуглил исходно такую регулярку: "[(\\[{](.*?)[)\\]}]"
+        Matcher m = p.matcher(this.line);
+        date_time = m.find() ? m.group() : "Not found";
+        date_time = date_time.replace(" +0300", "");
+        date_time = date_time.replace("[", "");
+        date_time = date_time.replace("]", "");
+        date_time = date_time.replace("/", ",");
+        System.out.println(date_time);
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd,MMM,yyyy:HH:mm:ss");
+        dl = LocalDateTime.parse(date_time, formatter);
+        return dl;
+    }
+
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,73 @@
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    static ArrayList<LogEntry> str = new ArrayList();
+    public static void main(String[] args) throws IOException {
+        String path = "C:\\Users\\pps_r\\IdeaProjects\\AccessLogParser\\access.log";
+        File file = new File(path);
+        boolean fileExists = file.exists();
+        boolean isDirectory = file.isDirectory();
+        if (fileExists == false) {
+            System.out.println("Ошибочно введен путь, или файл не существует");
+
+        }
+        if ((fileExists == true) && (isDirectory == true)) {
+            System.out.println("Это директория");
+
+        }
+        if ((fileExists == true) && (isDirectory == false)) {
+            BufferedReader reader = null;
+            try {
+                FileReader fileReader = new FileReader(path);
+                reader = new BufferedReader(fileReader);
+            } catch (FileNotFoundException ffe) {
+                System.out.println("FileNotFound");
+            }
+            String line;
+            int count = 0;
+            int countMozilla = 0;
+            int countGooglebot = 0;
+            int countYandexbot = 0;
+            int length = 0;
+            int i = 0;
+            while((line = reader.readLine()) != null) {
+                str.add(new LogEntry(line));
+                //System.out.println(((LogEntry)str.get(i)).ip()+ "..."+((LogEntry)str.get(i)).ldt());
+                System.out.println(((LogEntry)str.get(i)).ip());
+                if (line.matches(".*Mozilla.*")) {
+                    ++countMozilla;
+                }
+
+                if (line.matches(".*YandexBot.*")) {
+                    ++countYandexbot;
+                }
+
+                if (line.matches(".*Googlebot.*")) {
+                    ++countGooglebot;
+                }
+
+                if (length > 1024) {
+                    throw new RuntimeException("Строка не должна быть длиннее 1024 символа");
+                }
+
+                try {
+                    length = line.length();
+                    ++count;
+                    ++i;
+                } catch (RuntimeException var14) {
+                    System.out.println("String must be shorter than 1024 characters");
+                }
+            }
+            System.out.println("Количество строка: " + count);
+            System.out.println("Число вхождений Mozilla:" + countMozilla);
+            System.out.println("Число вхождений Googlebot:" + countGooglebot);
+            System.out.println("Число вхождений Yandexbot:" + countYandexbot);
+            System.out.println("Доля запросов от Гуглоботов:" + (double) countGooglebot / countMozilla);
+            System.out.println("Доля запросов от Яндексботов:" + (double) countYandexbot / countMozilla);
+
+        }
+
     }
 }
